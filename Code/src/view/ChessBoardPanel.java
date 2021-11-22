ed91f5cfad9e96439d2947b2f8b300aa03b559aa
ronghaixi <12112845@mail.sustech.edu.cn>
2021-11-22 20:04:09 +0800

    new

---
+++ Code/src/view/ChessBoardPanel.java
@@ -0,0 +1,70 @@
+package view;
+
+import components.ChessGridComponent;
+import model.ChessPiece;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ChessBoardPanel extends JPanel {
+    private final int CHESS_COUNT = 8;
+    private ChessGridComponent[][] chessGrids;
+
+    public ChessBoardPanel(int width, int height) {
+        this.setVisible(true);
+        this.setFocusable(true);
+        this.setLayout(null);
+        this.setBackground(Color.BLACK);
+        int length = Math.min(width, height);
+        this.setSize(length, length);
+        ChessGridComponent.gridSize = length / CHESS_COUNT;
+        ChessGridComponent.chessSize = (int) (ChessGridComponent.gridSize * 0.8);
+        System.out.printf("width = %d height = %d gridSize = %d chessSize = %d\n",
+                width, height, ChessGridComponent.gridSize, ChessGridComponent.chessSize);
+
+        initialChessGrids();//return empty chessboard
+        initialGame();//add initial four chess
+
+        repaint();
+    }
+
+    /**
+     * set an empty chessboard
+     */
+    public void initialChessGrids() {
+        chessGrids = new ChessGridComponent[CHESS_COUNT][CHESS_COUNT];
+
+        //draw all chess grids
+        for (int i = 0; i < CHESS_COUNT; i++) {
+            for (int j = 0; j < CHESS_COUNT; j++) {
+                ChessGridComponent gridComponent = new ChessGridComponent(i, j);
+                gridComponent.setLocation(j * ChessGridComponent.gridSize, i * ChessGridComponent.gridSize);
+                chessGrids[i][j] = gridComponent;
+                this.add(chessGrids[i][j]);
+            }
+        }
+    }
+
+    /**
+     * initial origin four chess
+     */
+    public void initialGame() {
+        chessGrids[3][3].setChessPiece(ChessPiece.BLACK);
+        chessGrids[3][4].setChessPiece(ChessPiece.WHITE);
+        chessGrids[4][3].setChessPiece(ChessPiece.WHITE);
+        chessGrids[4][4].setChessPiece(ChessPiece.BLACK);
+    }
+
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, this.getWidth(), this.getHeight());
+    }
+
+    public boolean canClickGrid(int row, int col, ChessPiece currentPlayer) {
+        //todo: complete this method
+        return true;
+    }
+}
