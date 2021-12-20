cdef5e9e5680e4889459392c0a245dbe3333ea08
ronghaixi <12112845@mail.sustech.edu.cn>
2021-12-20 13:42:34 +0800

    BUG completed!!!

---
+++ Code/src/test/Images.java
@@ -0,0 +1,13 @@
+package test;
+
+import javax.swing.*;
+import java.net.URL;
+
+public class Images {
+        public static URL bodyURL= Images.class.getResource("/images/Othello22.jpg");
+        public static ImageIcon bodyImg =new ImageIcon(bodyURL);
+
+}
+
+
+
